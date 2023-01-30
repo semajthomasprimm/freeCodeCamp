571e2edefe3a17e8558a7d43d903e6756be1de44
Semaj Primm <semajthomasprimm@gmail.com>
2023-01-30 15:06:34 -0500

    completed Jan 30 exercises

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -19,3 +19,27 @@ function urlSlug(title) {
     return title.split(" ").filter(word => word !== "").join("-").toLowerCase();
 }
 urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
+
+// January 30
+
+// Use the every Method to Check that Every Element in an Array Meets a Criteria
+function checkPositive(arr) {
+    return arr.every((currentValue) => { return currentValue > 0 });
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+// Use the some Method to Check that Any Elements in an Array Meet a Criteria
+function checkPositive(arr) {
+    return arr.some((currentValue) => { return currentValue > 0 });
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+// Introduction to Currying and Partial Application
+function add(x) {
+    return function(y){
+      return function (z){
+        return x + y + z;
+      }
+    }
+}
+add(10)(20)(30);
